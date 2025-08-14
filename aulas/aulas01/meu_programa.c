b6b12348e3d2b07bb3df568c330b8fb14be8917a
Thamara Freitas <2002enzofreitas@gmail.com>
2025-08-13 21:51:55 -0300

    codigos da aula01

---
+++ aulas/aulas01/meu_programa.c
@@ -0,0 +1,9 @@
+#include <stdio.h>          //inclusao de bibliotecas
+#include <math.h>
+
+// funcao principal
+int main() { 
+    printf("Boa noite!\n");
+
+    return 0;   // indica que terminou o programa 
+} 
